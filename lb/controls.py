595d775da2da1645f7be2ed906aafb92bbe76052
Eugene Pankov <john.pankov@gmail.com>
2020-04-01 00:40:58 +0200

    gpio ext

--- lb/controls.py
+++ lb/controls.py
@@ -1,12 +1,8 @@
 import pygame
 import threading
 import time
+import wiringpi
 from rx.subject import Subject
-
-try:
-    from RPi import GPIO
-except ImportError:
-    GPIO = None
 
 
 class Button:
@@ -14,16 +10,15 @@ class Button:
         self.pin = pin
         self.key = key
         self.press = Subject()
-        if GPIO:
-            GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            self.last = GPIO.input(pin)
+        wiringpi.pinMode(pin, 0)
+        wiringpi.pullUpDnControl(pin, 2)
+        self.last = 1 - wiringpi.digitalRead(self.pin)
 
     def update(self):
-        if GPIO:
-            v = GPIO.input(self.pin)
-            if v and not self.last:
-                self.press.on_next(None)
-            self.last = v
+        v = 1 - wiringpi.digitalRead(self.pin)
+        if v and not self.last:
+            self.press.on_next(None)
+        self.last = v
 
     def process_event(self, event):
         if event.type == pygame.KEYDOWN and event.key == self.key:
@@ -31,7 +26,7 @@ class Button:
 
 
 class Rotary:
-    def __init__(self, pin_clk, pin_dt, key_left=None, key_right=None):
+    def __init__(self, pin_dt, pin_clk, key_left=None, key_right=None):
         self.pin_clk = pin_clk
         self.pin_dt = pin_dt
         self.key_left = key_left
@@ -39,26 +34,27 @@ class Rotary:
         self.left = Subject()
         self.right = Subject()
         self.pending = None
-        if GPIO:
-            GPIO.setup(pin_clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-            GPIO.setup(pin_dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-            self.clk_last = GPIO.input(pin_clk)
+
+        wiringpi.pinMode(pin_clk, 0)
+        wiringpi.pullUpDnControl(pin_clk, 2)
+        wiringpi.pinMode(pin_dt, 0)
+        wiringpi.pullUpDnControl(pin_dt, 2)
+        self.clk_last = 1 - wiringpi.digitalRead(self.pin_clk)
 
     def update(self):
-        if GPIO:
-            clk = GPIO.input(self.pin_clk)
-            dt = GPIO.input(self.pin_dt)
-            if clk and not dt:
-                self.pending = 'left'
-            if not clk and dt:
-                self.pending = 'right'
-            if clk and dt:
-                if self.pending == 'left':
-                    self.left.on_next(None)
-                if self.pending == 'right':
-                    self.right.on_next(None)
-                self.pending = None
-            self.clk_last = clk
+        clk = 1 - wiringpi.digitalRead(self.pin_clk)
+        dt = 1 - wiringpi.digitalRead(self.pin_dt)
+        if clk and not dt:
+            self.pending = 'left'
+        if not clk and dt:
+            self.pending = 'right'
+        if clk and dt:
+            if self.pending == 'left':
+                self.left.on_next(None)
+            if self.pending == 'right':
+                self.right.on_next(None)
+            self.pending = None
+        self.clk_last = clk
 
     def process_event(self, event):
         if event.type == pygame.KEYDOWN and event.key == self.key_left:
@@ -71,9 +67,13 @@ class Controls(threading.Thread):
     def __init__(self, app):
         super().__init__(daemon=True)
         self.app = app
-        if GPIO:
-            GPIO.setmode(GPIO.BCM)
-        self.play_button = Button(pin=21, key=pygame.K_SPACE)
+
+        pin_base = 100
+        i2c_addr = 0x20
+        wiringpi.wiringPiSetup()
+        wiringpi.mcp23017Setup(pin_base, i2c_addr)
+
+        self.play_button = Button(21, key=pygame.K_SPACE)
         self.stop_button = Button(20, key=pygame.K_s)
         self.record_button = Button(16, key=pygame.K_r)
         self.clear_button = Button(19, key=pygame.K_c)
@@ -81,7 +81,7 @@ class Controls(threading.Thread):
         self.s_1_button = Button(17, key=pygame.K_1)
         self.s_2_button = Button(27, key=pygame.K_2)
         self.rotary_param = Rotary(13, 26, key_left=pygame.K_DOWN, key_right=pygame.K_UP)
-        self.rotary_value = Rotary(5, 6, key_left=pygame.K_LEFT, key_right=pygame.K_RIGHT)
+        self.rotary_value = Rotary(108, 109, key_left=pygame.K_LEFT, key_right=pygame.K_RIGHT)
         self.items = [
             self.play_button,
             self.stop_button,
